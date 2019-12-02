e7296d776944ca9669deada05b690915907271da
Cole Brokamp <cole.brokamp@gmail.com>
2019-12-02 17:49:07 -0500

    operate by applying over geometry listcol rather than row index
    - added missing and duplicated data for testing
    - added code for cleaning input data and merging back for output data

--- _site_lookup_functions.R
+++ _site_lookup_functions.R
@@ -4,34 +4,28 @@ library(sf)
 d_grid <- readRDS('schwartz_grid_geohashed.rds')
 
 ## setup test input data
-d_locs <-
+raw_data <-
   tibble::tribble(
             ~id,         ~lon,        ~lat,
             809089L, -84.69127387, 39.24710734,
             813233L, -84.47798287, 39.12005904,
             814881L, -84.47123583,  39.2631309,
+            814888L, -84.47123583,  39.2631309,
             799697L, -84.41741798, 39.18541228,
+            799697L,      NA     , 39.18541228,
             799698L, -84.41395064, 39.18322447,
-          ) %>%
-  st_as_sf(coords = c('lon', 'lat'), crs = 4326) %>%
+          )
 
-## create a row index on the input data
-d_locs$.row <- seq_len(nrow(d_locs))
+## prepare data for calculations
+raw_data$.row <- seq_len(nrow(raw_data))
 
-## takes row index (for d_locs) and outputs nearest site index
-get_closest_grid_site_index <- function(.row_index) {
-  query_point <- d_locs[d_locs$.row == .row_index, ]
-  query_gh6 <- lwgeom::st_geohash(query_point, precision = 6)
-  query_gh6_and_neighbors <- geohashTools::gh_neighbors(query_gh6, self = TRUE)
-  nearby_indices <- which(d_grid$gh6 %in% query_gh6_and_neighbors)
-  nearby_points <- d_grid[nearby_indices, ]
-  nearest_site_index <-
-    st_join(st_transform(query_point, 5072),
-            st_transform(nearby_points, 5072),
-            join = st_nearest_feature) %>%
-    pull('site_index')
-  return(nearest_site_index)
-}
+d <-
+  raw_data %>%
+  select(.row, lat, lon) %>%
+  na.omit() %>%
+  group_by(lat, lon) %>%
+  nest(.rows = c(.row)) %>%
+  st_as_sf(coords = c('lon', 'lat'), crs = 4326)
 
 ## use the below code to generate illustrative map *while within the function environment*
 ## geohashTools::gh_to_sf(query_gh6_and_neighbors) %>%
@@ -40,10 +34,31 @@ get_closest_grid_site_index <- function(.row_index) {
 ##   mapview::mapview(nearby_points) +
 ##   mapview::mapview(nearby_points[nearby_points$site_index == nearest_site_index, 'geometry'], color = 'green')
 
-## get_closest_grid_site_index(3)
-## map_dbl(d_locs$.row, get_closest_grid_site_index)
+get_closest_grid_site_index <- function(query_point) {
+  query_point <- st_sfc(query_point, crs = 4326)
+  query_gh6 <- lwgeom::st_geohash(query_point, precision = 6)
+  query_gh6_and_neighbors <- geohashTools::gh_neighbors(query_gh6, self = TRUE)
+  nearby_indices <- which(d_grid$gh6 %in% query_gh6_and_neighbors)
+  nearby_points <- d_grid[nearby_indices, ]
+  which_nearest <-
+    st_nearest_feature(st_transform(query_point, 5072),
+                       st_transform(nearby_points, 5072))
+  nearby_points %>%
+    slice(which_nearest) %>%
+    pull('site_index')
+}
+
+## get_closest_grid_site_index(query_point = pluck(d$geometry, 1))
 
 ## apply across all rows to get site indices
-d_locs <- d_locs %>%
-  mutate(site_index = CB::mappp(d_locs$.row, get_closest_grid_site_index)) %>%
+## add options for parallel and cache??
+d <- d %>%
+  mutate(site_index = CB::mappp(d$geometry, get_closest_grid_site_index)) %>%
   unnest(cols = c(site_index))
+
+## merge back on .row after unnesting .rows into .row
+d <- d %>%
+  unnest(cols = c(.rows)) %>%
+  st_drop_geometry()
+
+out <- left_join(raw_data, d, by = '.row')
